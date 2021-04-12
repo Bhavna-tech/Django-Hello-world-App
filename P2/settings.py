3287beb0c8aaba95ac5a9cb0daa658f38e2b9f6a
Bhavna-tech <67250223+Bhavna-tech@users.noreply.github.com>
2021-04-12 15:44:44 +0530

    security updates

--- P2/settings.py
+++ P2/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6@%qgp@h@k__27m&rlwn@em4k-%tw9m%*g21^=95$jo1f5qme&'
+SECRET_KEY = '---secret key---'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
