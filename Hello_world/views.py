62d24228fdb558f1f32bd5dd80c52a2ec050b125
Bhavna-tech <67250223+Bhavna-tech@users.noreply.github.com>
2021-04-12 14:45:42 +0530

    mentioned class and college

--- Hello_world/views.py
+++ Hello_world/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render, HttpResponse
 
 def show(request):
-    return HttpResponse('Hello World!')
+    return HttpResponse('<p>Hello World! <br> <br>\t I am Bhavna Latare a student of Government College of Engineering Jalgaon. <br> I am a pursuing Computer Engineering graduate studying in 3rd year. </p> ')
